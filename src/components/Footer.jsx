362a6a891d042f30e5bad791a3d185169f47628b
MacUser <macuser@MacUsers-MacBook-Pro.local>
2025-11-19 18:47:54 +0200

    Added footer page

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer className='bg-blue-600 text-white text-center py-4 mt-12'>
+        <p> ©️ 2025 Student Portal | All Rights Reservd</p>
+        
+     </footer>
+
+      
+
+  );
+}
+
+export default Footer;
