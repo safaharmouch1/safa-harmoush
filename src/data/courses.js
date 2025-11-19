a27ce5881463baebb2ef912721f96c165303d492
MacUser <macuser@MacUsers-MacBook-Pro.local>
2025-11-20 00:42:14 +0200

    Added courses page

---
+++ src/data/courses.js
@@ -0,0 +1,10 @@
+const courses = [
+    {id:1,title:"Web Development", descrption: "Learn HTML,CSS,JS,React"},
+    {id:2,title:"Data Structure", descrption:"Algorithms and efficiency"},
+    {id:3,title:"Networks", descrption:"Routing,Switching,protocols"},
+    {id:4,title:"Software Engineering", descrption:"Project life cycle & design"},
+    {id:5,title:"AU Basic", descrption:"Introduction to artificial intelligence"},
+
+];
+
+export default courses;
