e4cee40ab905545c1eff8c83c8dc63f56bb66160
MacUser <macuser@MacUsers-MacBook-Pro.local>
2025-11-19 20:08:38 +0200

    Added about page

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div className='max-w-4xl mx-auto p-8'>
+        <h1 className='text-3xl font-bold text-blue-600'>About The System</h1>
+
+        <p className='mt-4 text-gray-700 text-lg'>The student Portal is a responsive web platform that helps students and instructors manage academic information such as profiles, grades, and courses in a modern, user-friendly interface.</p>
+      
+    </div>
+  );
+}
+
+export default About;
